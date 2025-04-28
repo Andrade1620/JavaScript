5a662786723d1dfdf1e999ff1deb3b48cf6a144f
Calebe Andrade <calebe.correia@aluno.senai.br>
2025-04-28 11:04:05 -0500

    Commit para salvar alterações de ferragens-joao

--- ferragens-joao/script.js
+++ ferragens-joao/script.js
@@ -1,4 +1,13 @@
-function o() {
+function calcularpreco() {
     let escolhaProduto = document.getElementById("produtos").value;
-    let qtd = document.getElementById("qtd")
+    let QuantidadeDeProdutos = document.getElementById("quantidade").value;
+    console.log(precos[escolhaProduto]*QuantidadeDeProdutos)
+
+}
+
+MostrarResultado() {
+    //valores
+    let valor1 = 649.000;
+    let valor2 = 5.431.000;
+    let resultado = valor1 + valor2;
 }

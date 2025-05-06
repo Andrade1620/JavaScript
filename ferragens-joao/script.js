79ab44690a1cbdf59bab3bdd73c05ec4ca2947d1
vitoria! <vitoria.a.lima10@aluno.senai.br!>
2025-05-06 10:26:20 -0500

    alteraçao todo o codigo

--- ferragens-joao/script.js
+++ ferragens-joao/script.js
@@ -1,13 +1,32 @@
-const precos={ //DICIONARIOP
-    "Challenger V8 R/T 1971": 649000,
-    "Lamborghini Aventador SVJ 2018": 5431000,
-}
-
-
-
-function calcularpreco() {
-    let escolhaProduto = document.getElementById("Produtos").value;
-    let QuantidadeDeProdutos = document.getElementById("Quantidade").value;
-    document.getElementById("resultado").innerHTML = precos[escolhaProduto]*QuantidadeDeProdutos;
+// Preços dos produtos
+const precos = {
+    'parafuso': 0.5,
+    'martelo': 25,
+    'chave de fenda': 10
 
 }
+ // Estoques atualizados
+ const estoque = {
+    "parafuso": 90,
+    "martelo": 5,
+    "chave de fenda": 12
+};
+
+function calcularPreco() {
+    const produto = document.getElementById('produto').value;
+    const quantidade = parseInt(document.getElementById('quantidade').value);
+
+    if (!produto || isNaN(quantidade) || quantidade <= 0) {
+        document.getElementById('resultado').innerText = 'Por favor, selecione um produto e insira uma quantidade válida.';
+        return; //encerrando execução
+    }
+
+    if (quantidade > estoque[produto]) {
+        document.getElementById('resultado').innerText = Quantidade disponivel em estoque. Disponíve:  ${estoque[produto]};
+        return; //encerrar a execução
+    }
+
+
+    const precoTotal = precos[produto] * quantidade; //acessando a posção e calculando preço
+
+    document.getElementById('resultado').innerText = Total: R$ ${precoTotal.toFixed(2)}; //acessando h4 e alterando valor
